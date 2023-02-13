7e3a972842686ab3a1b46b1b89d3ea36f12d2530
whyrexpsh <masha.krasheninnikova@gmail.com>
2023-02-14 00:46:14 +0400

    fixxxx

--- public/js/client.js
+++ public/js/client.js
@@ -1,20 +1,34 @@
 console.log('Client-side code running');
 
-const button = document.getElementById('searchPwnage');
-button.addEventListener('click', function(e) {
-  console.log('button was clicked');
+function GetData() {
+  const uri = 'http://25.68.246.17:30001/api/search';
+  const initDetails = {
+    mode: "cors",
+    method: "post",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    //make sure to serialize your JSON body
+    body: JSON.stringify({"key":"email","value":"clairelaigo@yahoo.com"})
+    }
 
-fetch("http://25.68.246.17:30001/api/search", {
-  method: "post",
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
+  fetch(uri, initDetails).then( response => {
+    if ( response.status !== 200 )
+    {
+        console.log( 'Looks like there was a problem. Status Code: ' +
+            response.status );
+        return;
+    }
 
-  //make sure to serialize your JSON body
-  body: JSON.stringify({"key":"email","value":"clairelaigo@yahoo.com"})
-  })
-  .then( (response) => { 
-   console.log("Success");
-});
-});
+    console.log( response.headers.get( "Content-Type" ) );
+    return response.json();
+  }).then( myJson =>
+                {
+                    console.log( JSON.stringify( myJson ) );
+                } )
+                .catch( err =>
+                {
+                    console.log( 'Fetch Error :-S', err );
+                } );
+}
